442a63241945c980a413533043f6e1d01a880d65
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-18 16:16:20 -1000

    fullly animated bubble sort animation

--- js/bubble-sort.js
+++ js/bubble-sort.js
@@ -10,24 +10,10 @@ function BubbleDOMAnimator() {
     }
     return widthArr;
   }
-
   //////////////////DOM Main Elements and Nodes//////////////////
   var mainContainer = document.getElementById('mainContainer');
   var i;
   //////////////////////////////////////////////////////////////
-
-
-
-  this.init = function() {
-
-    for (i = 0; i < 100; i++) {
-      var row = document.createElement('div');
-      row.id = 'row' + i;
-      row.style.width = ((1 + i) * 2) + 'px';
-      row.className = 'bubblerow';
-      mainContainer.appendChild(row);
-    }
-  }
 
   this.randomize = function() {
     var widthArr = generateLength();
@@ -43,21 +29,17 @@ function BubbleDOMAnimator() {
 
   this.sort = function() {
     var base, next;
-    for (var i = 0; i < 100 - 0; i++) {
-      for (var j = 0; j < 99; j++) {
-        base = document.getElementById('row' + j).style.width;
-        // console.log('base' + base);
+    for (var j = 0; j < 99; j++) {
+      base = document.getElementById('row' + j).style.width;
+      next = document.getElementById('row' + (j + 1)).style.width;
 
-        next = document.getElementById('row' + (j + 1)).style.width;
-        
-        // console.log('next' + next);
-        if (parseInt(base) > parseInt(next)) {
-          document.getElementById('row'+j).style.width = next;
-          document.getElementById('row'+(j+1)).style.width = base;
-        }
+      if (parseInt(base) > parseInt(next)) {
+        document.getElementById('row' + j).style.width = next;
+        document.getElementById('row' + (j + 1)).style.width = base;
       }
     }
-
-
+  }
+  this.start = function(){
+    setInterval(this.sort,100);
   }
 }
