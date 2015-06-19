a3ff0d744ad10d203bfa51f29043816e051629da
kawikadkekahuna <kawikakekahuna@gmail.com>
2015-06-19 10:18:30 -1000

    Create layout-sort.js
    added layout.js generator

---
+++ js/layout-sort.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+  var bSort = new BubbleDOMAnimator();
+  var main = document.getElementById('mainContainer');
+  var rowContainer = document.getElementById('rowContainer');
+  var inputField = document.createElement('input');
+  inputField.setAttribute('type', 'text');
+  inputField.id = 'inputField';
+
+  var generateBtn = document.createElement('div');
+  generateBtn.innerHTML = "Generate";
+  generateBtn.id = 'generateBtn';
+  generateBtn.addEventListener('click', function() {
+    bSort.randomize(inputField.value);
+  });
+
+  var startBtn = document.createElement('div');
+  startBtn.innerHTML = 'START!';
+  startBtn.id = 'startBtn';
+  startBtn.addEventListener('click', function() {
+    bSort.start();
+  });
+
+
+var resetBtn = document.createElement('div');
+resetBtn.innerHTML = 'Reset';
+resetBtn.addEventListener('click', function() {
+
+  bSort.reset();
+  
+});
+
+  main.appendChild(resetBtn);
+  main.appendChild(inputField);
+  main.appendChild(startBtn);
+  main.appendChild(generateBtn);
+
+}
